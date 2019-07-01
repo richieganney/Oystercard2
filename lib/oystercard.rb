4724e120078c63398b74e5c72780e8735508c517
hjdr <harry.jdr@gmail.com>
2019-07-01 15:57:58 +0100

    added Oystercard.top_up with balance limit

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -2,12 +2,19 @@ class Oystercard
 
 attr_accessor :balance
 
+BALANCE_LIMIT = 90
+
   def initialize(balance=0)
     @balance = balance
   end
 
   def top_up(amount)
-    self.balance += 20.00
+    raise("amount exceeded, balance cannot be: #{amount + self.balance}. Balance limit is 90") if balance_check(amount)
+    self.balance += amount
+  end
+
+  def balance_check(amount)
+    amount + self.balance > BALANCE_LIMIT
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -10,8 +10,16 @@ describe Oystercard do
 
   describe "#top_up" do
     it "Add money to the balance" do
-      subject.top_up(20.00)
-      expect(subject.balance).to eq 20.00
+      subject.top_up(20)
+      expect(subject.balance).to eq 20
     end
   end
+  
+  describe "#top_up" do
+    it "raises an error if combined balance > 90.00" do
+      card = Oystercard.new
+      expect { card.top_up(95) }.to raise_error("amount exceeded, balance cannot be: 95. Balance limit is 90")
+    end
+  end
+
 end
