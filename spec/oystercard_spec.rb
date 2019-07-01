94a7df7c609bf84c452e50d9452ae9a1462012e3
Student <student@Makerss-MacBook-Pro.local>
2019-07-01 14:55:07 +0100

    balance added

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,2 +1,10 @@
-describe Oyster do
+require 'oystercard'
+
+describe Oystercard do
+
+  describe "#balance" do
+    it "Set balance to 0 on initialize" do
+      expect(subject.balance).to eq 0
+    end
+  end
 end
