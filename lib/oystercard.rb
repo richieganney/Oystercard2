1b139657eb3a98beb2d93e14510eff1c143cb81c
Student <student@Makerss-MacBook-Pro.local>
2019-07-04 09:50:39 +0100

    step 14 start again

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,5 +1,5 @@
 class OysterCard
-  attr_reader :balance, :journeys
+  attr_reader :balance, :journeys, :entry_station, :exit_station
   DEFAULT_LIMIT = 90
   MINIMUM_FARE = 1
 
@@ -13,9 +13,29 @@ class OysterCard
     @balance += amount
   end
 
+  def touch_in(entry_station)
+    raise "Insufficent funds" if @balance < MINIMUM_FARE
+    @entry_station = entry_station
+  end
+
+  def touch_out(entry_station, exit_station)
+    deduct(MINIMUM_FARE)
+    @journeys << {entry_station: entry_station, exit_station: exit_station}
+    @entry_station = nil
+  end
+
+  def journey?
+    !!entry_station
+  end
+
   private
 
   def deduct(amount)
     @balance -= amount
   end
 end
+
+
+class Journey
+
+end 
