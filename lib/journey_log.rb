4160e0d491774fda77a0a374f87870e5df0aed05
Richie <richieganney@gmail.com>
2019-07-05 16:31:08 +0100

    Final step. Feature tests working for more complex fare functionality. Still need to write more unit tests for fare method

--- lib/journey_log.rb
+++ lib/journey_log.rb
@@ -8,4 +8,10 @@ class JourneyLog
     @all_journeys
   end
 
+  def fare
+    h = [@all_journeys[-1].values[1], @all_journeys[-1].values[3]].sort
+    (h[-1] - h[0]) + 1
+  end
+
 end
+
--- spec/journey_log_spec.rb
+++ spec/journey_log_spec.rb
@@ -1,7 +1,14 @@
 require 'journey_log'
 
 describe JourneyLog do
-  
+  subject(:instance_of_journey_log) { described_class.new }
     it { is_expected.to respond_to(:log_journey) }
 
+    describe '#fare' do
+      it 'calculates the correct fare according to number of zones crossed' do
+        journey_log = double(:journey_log)
+        journey_log = [{:entry_station_name=>"hammersmith", :entry_station_zone=>3, :exit_station_name=>"high biz", :exit_station_zone=>6}]
+        expect(journey_log.fare).to eq 4      
+      end
+    end
 end
